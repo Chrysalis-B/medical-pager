4e0a5d2807eb2b78dbe81ea1cc3b3d333323137c
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-02-07 19:19:08 +0100

    Add auth routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import authRoutes from './routes/auth.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,5 +13,7 @@ app.use(express.urlencoded());
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
