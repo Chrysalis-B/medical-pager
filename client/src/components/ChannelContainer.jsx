f967a25b26d2581e1b1e8e280060deefca030a70
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-02-10 18:18:57 +0100

    Add channel empty state and stream channel component to channel container

--- client/src/components/ChannelContainer.jsx
+++ client/src/components/ChannelContainer.jsx
@@ -27,9 +27,21 @@ const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, 
     )
   }
 
+  const EmptyState = () => (
+    <div className='channel-empty__container'>
+      <p className='channel-empty__first'>This is the beginning of your chat history.</p>
+      <p className='channel-empty__second'>Send messages, attachments, links, emojis, and more!</p>
+    </div>
+  );
+
   return (
-    <div>
-      ChannelContainer
+    <div className='channel__container'>
+      <Channel
+        EmptyStateIndicator={EmptyState}
+        Message={(messageProps, key) => <TeamMessage key={key} {...messageProps} />}
+      >
+        <ChannelInner setIsEditing={setIsEditing} />
+      </Channel>
     </div>
   );
 };
